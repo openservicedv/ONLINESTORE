455b2a8050a652300d64ffee8fc72989ea16770a
openservicedv <150409269+openservicedv@users.noreply.github.com>
2024-01-23 21:29:24 -0800

    redux + local Storage + combineReducer 2

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import {legacy_createStore, combineReducers} from 'redux';
 import {storeReducer} from './storeReducer';
 import {cartReducer} from './cartReducer';
+import {composeWithDevTools} from '@redux-devtools/extension';
+// import {applyMiddleware} from 'redux';
 
 const rootReducer = combineReducers({
 	cartReducer,
 	storeReducer
 });
-export const store = legacy_createStore(rootReducer);
+export const store = legacy_createStore(rootReducer, composeWithDevTools());
 
 
